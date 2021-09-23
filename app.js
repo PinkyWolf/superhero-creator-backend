b3c87ae3fb1272dda201104478685bae274b601e
Serhii Hult <gyltsergiy@gmail.com>
2021-09-23 22:47:20 +0300

    minor changes

--- app.js
+++ app.js
@@ -2,7 +2,11 @@ const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
 
+
 const contactsRouter = require('./routes/api/contacts')
+const usersRouter = require('./routes/api/users')
+const {HttpCode} = require('./helpers/constants')
+
 
 const app = express()
 
@@ -13,13 +17,14 @@ app.use(cors())
 app.use(express.json())
 
 app.use('/api/contacts', contactsRouter)
+app.use('/api/users', usersRouter)
 
 app.use((req, res) => {
-  res.status(404).json({ status: 'error', code: 404, message: 'Not found' })
+  res.status(HttpCode.NOT_FOUND).json({ status: 'error', code: HttpCode.NOT_FOUND, message: 'Not found' })
 })
 
 app.use((err, req, res, next) => {
-  const status = err.status || 500
+  const status = err.status || HttpCode.INTERNAL_SERVER_ERROR
   res.status(status).json({ status: 'fail', code: status, message: err.message })
 })
 
